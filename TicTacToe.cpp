33dd5fcb96e5259b5bde6c8239cebda40a94d187
anhthy95 <51308518+anhthy95@users.noreply.github.com>
2020-09-24 21:45:03 -0600

    Revert "Merge branch 'master' of https://github.com/anhthy95/PE-04-TicTacToe into master"
    This reverts commit 8cc8c9f8106b273453929794a6a6dc2086c31621, reversing
    changes made to 1c58256d6026a7670367e192b7d8c8ff1761f58b.

---
+++ TicTacToe.cpp
@@ -0,0 +1,34 @@
+//Name: Person 1: Thy Nguyen 
+//		Person 2: TianWei Zhao
+//CSCI 3010 - PE 04 - Tic Tac Toe
+
+//header
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include <sstream>
+#include <stdio.h>
+#include <ctype.h>
+
+//linked file
+#include "TicTacToe.h"
+
+
+using namespace std;
+
+
+//-----------FUNCTIONS-------------//
+
+/** 
+	Create an "empty" 3x3 matrix
+*/
+
+std::vector<Cell> TicTacToe::CreateBoard(){
+	for(int row = 0; row < 4; row++){
+		for(int col = 0; col < 4; col++){
+			board_[row][col] = Cell::Empty;
+		}
+	}
+}
+
---
+++ TicTacToe.h
@@ -0,0 +1,30 @@
+#ifndef _TICTACTOEBOARD_
+#define _TICTACTOEBOARD_
+
+/** 
+	Cell enum to represent spaces on the board
+*/
+
+
+enum class Cell {X, O, Empty};
+
+
+/**
+	Class for 3x3 Tic Tac Toe board game
+*/
+
+
+class TicTacToe {
+	
+public:
+	std::vector<Cell> CreateBoard(); // Create an empty 3 x 3 board.
+
+private:
+	Cell board_[3][3];
+	
+};
+
+#endif
+
+
+
